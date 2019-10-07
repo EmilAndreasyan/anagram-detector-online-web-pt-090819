5982ce5e5eb64acdee1e3d27df434ea91c9d5e6c
Emil Andreasyan <emil.andreasyan@yahoo.com>
2019-10-07 05:23:38 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :match, :anagram, :word
+  def initialize(word)
+    @word = word
+  end
+  
+  def match(words)
+    @anagram = []
+    words.each do |el|
+    if @word.split("") == el.split("")
+    @anagram << el
+    end
+    end
+    @anagram
+  end
+
+end
+
+listen = Anagram.new("listen")
+listen.match(%w(enlists google inlets banana))
